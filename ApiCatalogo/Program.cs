87f603df1c0343faa9fc9f4f2b917b3c8f1886ea
Kayo Rafael <kayorafaelpj@outlook.com>
2026-03-10 15:31:24 -0400

    feat: definido relacionamento 1:N entre Categoria e Produto

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,16 +1,20 @@
+using ApiCatalogo.Context;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+                        options.UseMySql(mySqlConnection,
+                        ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
-
+    
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
