54436931074c720f33c92e83d80d08316f450e33
Kayo Rafael <kayorafaelpj@outlook.com>
2026-03-11 13:37:25 -0400

    feat: adiciona data annotations para validar modelos e configurar o banco

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiCatalogo.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
+
+    [Key] // Chave Primaria
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+
     public ICollection<Produto>? Produtos { get; set; }
 }
